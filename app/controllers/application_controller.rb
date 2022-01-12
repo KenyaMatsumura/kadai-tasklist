bb678e2a987b43719597532d98c4908a41655774
kenya.matsumura <arupaka@au.com>
2022-01-12 17:31:28 +0000

    Lesson20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper #logged_in? 使用のため継承
   include Pagy::Backend
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
